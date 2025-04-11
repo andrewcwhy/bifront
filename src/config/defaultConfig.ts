580578bfb2d2af620d5e3b081ec92963335ea375
andrewcwhy <acydeveloper05@gmail.com>
2025-04-10 23:16:18 -0400

    Updates

---
+++ src/config/defaultConfig.ts
@@ -0,0 +1,13 @@
+import { BifrontConfig } from '../types/config';
+  
+  export const defaultConfig: BifrontConfig = {
+    port: 9705,
+    sidebar: {
+        initalState: 'expanded',
+        sort: 'asc',
+    },
+    fileExplorer: {
+      showHiddenFiles: false,
+      excludePatterns: ['node_modules', '.git', 'dist', 'build'],
+    },
+  };
